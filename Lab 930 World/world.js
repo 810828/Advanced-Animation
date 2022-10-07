ce9e110999cd7b86d34d0ec6afd7c7089de230c1
Malcolm McDonald <810828@seq.org>
2022-10-07 10:54:29 -0700

    ..

--- Lab 930 World/world.js
+++ Lab 930 World/world.js
@@ -14,13 +14,13 @@ function World() {
   };
 
   this.movers = [];
-  this.loadMovers();
+  this.loadMovers(5);
 
   //Step 1::reduce world to fit inside of mini Canvas
   this.scaleX = 10;
   this.scaleY = 10;
-  this.cnvMainLoc = new JSVector(0, 0);
 
+  this.cnvMainLoc = new JSVector(0, 0);
   // add an event handler such that the a, s, w, d keys
   // will reposition the canvas within the world.
   window.addEventListener(
@@ -56,24 +56,31 @@ function World() {
 
 // run the world in animation
 World.prototype.run = function () {
-  let ctx = this.ctxMain;
-  ctx.save();
-  ctx.beginPath();
-  ctx.translate(-this.cnvMainLoc.x, -this.cnvMainLoc.y);
-  ctx.moveTo(0, this.dims.top); // draw the y axis
-  ctx.lineTo(0, this.dims.bottom); // x = 0
-  ctx.moveTo(this.dims.left, 0);
-  ctx.lineTo(this.dims.right, 0);
-  ctx.strokeStyle = "blue";
-  ctx.stroke();
+  this.ctxMain.save();
+  this.ctxMain.beginPath();
+  this.ctxMain.translate(-this.cnvMainLoc.x, -this.cnvMainLoc.y);
+  this.ctxMain.moveTo(0, this.dims.top); // draw the y axis
+  this.ctxMain.lineTo(0, this.dims.bottom); // x = 0
+  this.ctxMain.moveTo(this.dims.left, 0);
+  this.ctxMain.lineTo(this.dims.right, 0);
+  this.totalWidth = -this.dims.left + this.dims.right;
+  this.totalHeight = -this.dims.top + this.dims.bottom;
+  this.ctxMain.clearRect(
+    this.dims.left,
+    this.dims.top,
+    this.totalWidth,
+    this.totalHeight
+  );
+  this.ctxMain.strokeStyle = "blue";
+  this.ctxMain.stroke();
 
   // Step Two:  Move cnvMain in the world and run movers  ########################################################
   //  Clear the rectangle in the main Canvas
-  ctx.clearRect(0, 0, -1500, 1500);
 
   //  move the main canvas inside of the world
 
   //  scale the world to fit into the miniCanvas
+  this.ctxMini.scale(this.scaleX, this.scaleY);
 
   //  center the world inside of the miniCanvas
 
@@ -84,7 +91,7 @@ World.prototype.run = function () {
   }
 
   //  restore the context
-  ctx.restore();
+  this.ctxMain.restore();
 
   // Step Three:  Draw the mainCanv and axes in the miniCanv ########################################################
   //    scale cnvMini to contain the entire world
@@ -100,7 +107,11 @@ World.prototype.run = function () {
 };
 
 //Load mover array
-World.prototype.loadMovers = function () {};
+World.prototype.loadMovers = function (n) {
+  for (let i = 0; i < n; i++) {
+    this.movers[i] = new Mover();
+  }
+};
 
 World.prototype.getRandomColor = function () {
   //  List of hex color values for movers
